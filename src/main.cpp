b984793db2aea10ce2833661cbb9a8dc09172999
Mateusz Wojtyna <w_mateusz@hotmail.com>
2024-07-29 01:32:24 +0200

    feat: draw color with opengl

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,34 @@
 #include <iostream>
 #include <ostream>
 
+#ifdef __APPLE__
+#define GL_SILENCE_DEPRECATION
+#endif
+
 int main() {
-    std::cout << glfwGetVersionString() << std::flush;
+    std::cerr << glfwGetVersionString() << std::endl;
 
     glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(640, 480, "yate", nullptr, nullptr);
+    GLFWwindow *window = glfwCreateWindow(640, 480, "yate", nullptr, nullptr);
     if (window == nullptr) {
+        const char *error;
+        glfwGetError(&error);
+        std::cerr << "Failed to create window: " << error << std::endl;
+
         glfwTerminate();
-        return 1;
+        return -1;
     }
 
     glfwMakeContextCurrent(window);
 
     while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
