b8e2759d378235756660f67776a23581b4fd0dea
Mateusz Wojtyna <w_mateusz@hotmail.com>
2024-09-04 19:01:43 +0200

    refactor: move private member functions from header to cpp

--- src/rendering/program.cpp
+++ src/rendering/program.cpp
@@ -1,4 +1,5 @@
 #include "program.hpp"
+#include "../utils.hpp"
 #include "opengl.hpp"
 #include <assert.h>
 #include <cstdlib>
@@ -52,6 +53,7 @@ void Program::setUniformFloat(const GLchar* const name, const float value) {
     use();
     const GLint location = getUniformLocation(name);
     glCall(glUniform1f(location, value));
+    SPDLOG_TRACE("Set uniform '{}'", name);
 }
 
 void Program::setUniformMatrix4(const GLchar* const name,
@@ -59,4 +61,45 @@ void Program::setUniformMatrix4(const GLchar* const name,
     use();
     const GLint location = getUniformLocation(name);
     glCall(glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(mat)));
+    SPDLOG_TRACE("Set uniform '{}'", name);
+}
+
+// PRIVATE
+GLuint Program::loadShader(const std::string contents, const GLuint type) {
+    assert(contents.length() > 0);
+    const GLchar* const string = contents.c_str();
+
+    glCall(GLuint shaderId = glCreateShader(type));
+    glCall(glShaderSource(shaderId, 1, &string, nullptr));
+    glCall(glCompileShader(shaderId));
+
+    GLint compiled = GL_FALSE;
+    glCall(glGetShaderiv(shaderId, GL_COMPILE_STATUS, &compiled));
+    if (compiled != GL_TRUE) {
+        GLint logLen = 0;
+        glCall(glGetShaderiv(shaderId, GL_INFO_LOG_LENGTH, &logLen));
+
+        GLchar* log = (GLchar*)std::malloc(logLen);
+        glCall(glGetShaderInfoLog(shaderId, logLen, 0, log));
+        FATAL("Shader with id={} failed to compile:\n{}", shaderId, log);
+        std::free(log);
+    }
+
+    SPDLOG_DEBUG("Compiled shader with id={}", shaderId);
+    return shaderId;
+}
+
+GLint Program::getUniformLocation(const GLchar* const name) {
+    if (m_UniformLocations.contains(name)) {
+        return m_UniformLocations.at(name);
+    } else {
+        use();
+        glCall(const GLint location = glGetUniformLocation(m_Id, name));
+        if (location == -1) {
+            SPDLOG_ERROR("Uniform named '{}' not found", name);
+            return -1;
+        }
+        m_UniformLocations[name] = location;
+        return location;
+    }
 }
--- src/rendering/program.hpp
+++ src/rendering/program.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "../utils.hpp"
 #include "opengl.hpp"
 #include <glm/ext/matrix_float4x4.hpp>
 #include <spdlog/spdlog.h>
@@ -21,42 +20,6 @@ private:
     /// name -> location
     std::unordered_map<const GLchar*, GLint> m_UniformLocations;
 
-    GLuint loadShader(const std::string contents, const GLuint type) {
-        assert(contents.length() > 0);
-        const GLchar* const string = contents.c_str();
-
-        glCall(GLuint shaderId = glCreateShader(type));
-        glCall(glShaderSource(shaderId, 1, &string, nullptr));
-        glCall(glCompileShader(shaderId));
-
-        GLint compiled = GL_FALSE;
-        glCall(glGetShaderiv(shaderId, GL_COMPILE_STATUS, &compiled));
-        if (compiled != GL_TRUE) {
-            GLint logLen = 0;
-            glCall(glGetShaderiv(shaderId, GL_INFO_LOG_LENGTH, &logLen));
-
-            GLchar* log = (GLchar*)std::malloc(logLen);
-            glCall(glGetShaderInfoLog(shaderId, logLen, 0, log));
-            FATAL("Shader with id={} failed to compile:\n{}", shaderId, log);
-            std::free(log);
-        }
-
-        SPDLOG_DEBUG("Compiled shader with id={}", shaderId);
-        return shaderId;
-    }
-
-    GLint getUniformLocation(const GLchar* const name) {
-        if (m_UniformLocations.contains(name)) {
-            return m_UniformLocations.at(name);
-        } else {
-            use();
-            glCall(const GLint location = glGetUniformLocation(m_Id, name));
-            if (location == -1) {
-                SPDLOG_ERROR("Uniform named '{}' not found", name);
-                return -1;
-            }
-            m_UniformLocations[name] = location;
-            return location;
-        }
-    }
+    GLuint loadShader(const std::string contents, const GLuint type);
+    GLint getUniformLocation(const GLchar* const name);
 };
