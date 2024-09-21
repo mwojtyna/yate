4ba6715985f9ea7a57286c1a0ee278e69a0c68c3
Mateusz Wojtyna <w_mateusz@hotmail.com>
2024-09-21 22:06:04 +0200

    refactor: share setup code for tests

--- src/tests/osc.cpp
+++ src/tests/osc.cpp
@@ -4,40 +4,38 @@
 #include <string>
 #include <vector>
 
-TEST_CASE("OSC 0 - set window title") {
+TEST_CASE("OSC", "[osc]") {
     bool wasCalled = false;
     CsiParser csi;
-
     OscParser osc;
-    osc.addHandler(0, [&wasCalled](std::vector<std::string> args) {
-        wasCalled = true;
-        REQUIRE(args.size() == 1);
-        REQUIRE(args[0] == "title");
-    });
 
-    std::vector<uint8_t> data = {c0::ESC, c0::OSC, '0', ';', 't',
-                                 'i',     't',     'l', 'e', c0::BEL};
-    Parser parser(std::move(csi), std::move(osc));
-    parser.parse(data);
+    SECTION("OSC 0 - set window title") {
+        osc.addHandler(0, [&wasCalled](std::vector<std::string> args) {
+            wasCalled = true;
+            REQUIRE(args.size() == 1);
+            REQUIRE(args[0] == "title");
+        });
 
-    REQUIRE(wasCalled);
-}
+        std::vector<uint8_t> data = {c0::ESC, c0::OSC, '0', ';', 't',
+                                     'i',     't',     'l', 'e', c0::BEL};
+        Parser parser(std::move(csi), std::move(osc));
+        parser.parse(data);
 
-TEST_CASE("OSC 2 - set window title") {
-    bool wasCalled = false;
-    CsiParser csi;
+        REQUIRE(wasCalled);
+    }
 
-    OscParser osc;
-    osc.addHandler(2, [&wasCalled](std::vector<std::string> args) {
-        wasCalled = true;
-        REQUIRE(args.size() == 1);
-        REQUIRE(args[0] == "title");
-    });
+    SECTION("OSC 2 - set window title") {
+        osc.addHandler(2, [&wasCalled](std::vector<std::string> args) {
+            wasCalled = true;
+            REQUIRE(args.size() == 1);
+            REQUIRE(args[0] == "title");
+        });
 
-    std::vector<uint8_t> data = {c0::ESC, c0::OSC, '2', ';', 't',
-                                 'i',     't',     'l', 'e', c0::BEL};
-    Parser parser(std::move(csi), std::move(osc));
-    parser.parse(data);
+        std::vector<uint8_t> data = {c0::ESC, c0::OSC, '2', ';', 't',
+                                     'i',     't',     'l', 'e', c0::BEL};
+        Parser parser(std::move(csi), std::move(osc));
+        parser.parse(data);
 
-    REQUIRE(wasCalled);
+        REQUIRE(wasCalled);
+    }
 }
